3e260365655bd49b4e31180fb224bce4357bc0b6
gortron <gordon.lanza@gmail.com>
2020-01-11 17:05:20 +0000

    Pledges now accept amount as params

--- app/controllers/pledge_controller.rb
+++ app/controllers/pledge_controller.rb
@@ -12,16 +12,17 @@ class PledgeController < ApplicationController
     # Testing Stripe API, should incorporate form information from the post invoked in the front-end
     user = User.find_or_create_by(email: params[:user_email])
     project = Project.find_by(name: params[:project_name])
+    amount = params[:amount]
 
     token = params[:stripe_token]
     charge = Stripe::Charge.create({
-      amount: 1000,
+      amount: amount,
       currency: 'usd',
       description: 'Charge for jenny.rosen@example.com',
       receipt_email: 'jenny.rosen@example.com',
       source: token
     })
 
-    Pledge.create(amount: 1000, user_id: user.id, project_id: project.id)
+    Pledge.create(amount: amount, user_id: user.id, project_id: project.id)
   end
 end
